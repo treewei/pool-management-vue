9a2abf9cdb14f4460e718e53aea79cc009bf0d9b
jiajiwei <jiajiwei@jiujiutech.cn>
2020-09-24 14:53:03 +0800

    [feat]add ropsten env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,19 +1,25 @@
 import merge from 'lodash/merge';
 import registry from 'assets/generated/pm/registry.homestead.json';
 import registryKovan from 'assets/generated/pm/registry.kovan.json';
+import registryRopsten from '@/config/registry.ropsten.json';
 import homestead from '@/config/homestead.json';
 import staging from '@/config/homestead.staging.json';
 import kovan from '@/config/kovan.json';
-import { clone } from '@/helpers/utils';
+import ropsten from '@/config/ropsten.json';
+import {clone} from '@/helpers/utils';
 
 const configs = {
-  production: { homestead, kovan },
-  staging: { homestead: merge(clone(homestead), staging), kovan }
+    production: {homestead, kovan, ropsten},
+    staging: {homestead: merge(clone(homestead), staging), kovan, ropsten}
 };
 configs.production.homestead = merge(registry, configs.production.homestead);
 configs.production.kovan = merge(registryKovan, configs.production.kovan);
+configs.production.ropsten = merge(registryRopsten, configs.production.ropsten);
+
 configs.staging.homestead = merge(registry, configs.staging.homestead);
 configs.staging.kovan = merge(registryKovan, configs.staging.kovan);
+configs.staging.ropsten = merge(registryRopsten, configs.staging.ropsten);
+
 const env = process.env.VUE_APP_ENV || 'production';
 const network = process.env.VUE_APP_NETWORK || 'homestead';
 
